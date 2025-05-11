a3473e5ee573598018f02c1a194ef10f1275d2a0
Jakub <pecet3107@gmail.com>
2025-05-11 12:45:06 +0200

    created config

--- src/config.go
+++ src/config.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type ConfigServer struct {
+	URL       string `yaml:"url"`
+	StatusURL string `yaml:"statusURL"`
+}
+
+type Config struct {
+	Port           int             `yaml:"port"`
+	StatusInterval int             `yaml:"statusInterval"`
+	Servers        []*ConfigServer `yaml:"servers"`
+}
+
+func GetConfig() (*Config, error) {
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		data, err = os.ReadFile("config.yml")
+		if err != nil {
+			return nil, err
+		}
+	}
+	cfg := &Config{}
+	err = yaml.Unmarshal(data, cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
--- src/main.go
+++ src/main.go
@@ -8,7 +8,13 @@ import (
 func main() {
 	http.HandleFunc("/", handler)
 	log.Println("Starting a loadbalancer :8080...")
+	cfg, err := GetConfig()
+	if err != nil {
+		log.Fatalln("Error with loading a config file!")
+	}
+	log.Println(cfg.Port)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("Błąd serwera: ", err)
 	}
+
 }
